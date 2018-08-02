65202d54b11f781dfecc98bf671b3cce86934275
natwit <natwit442@gmail.com>
2018-08-03 00:54:04 +0700

    I just adddef an Init inside the class Person

--- testing.swift
+++ testing.swift
@@ -1,1 +1,15 @@
-
+class People{
+    var firstName:String
+    var lastName: String
+    var fullName:String
+    var dob:String
+    
+    init(firstName: String, lastName:String, dob:String) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.fullName = fullName + " " + lastName
+        self.dob = dob
+    }
+    
+    
+}
